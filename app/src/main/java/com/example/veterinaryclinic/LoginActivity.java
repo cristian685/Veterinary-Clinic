9f053b46c3c0a9b72f1f8215676ea35cdeec686c
Cristian <cristi.popina98@gmail.com>
2020-08-27 16:10:42 +0300

    Add user name and password edit text together with the messages and login button

--- app/src/main/java/com/example/veterinaryclinic/LoginActivity.java
+++ app/src/main/java/com/example/veterinaryclinic/LoginActivity.java
@@ -3,6 +3,10 @@ package com.example.veterinaryclinic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText userNameEditText = findViewById(R.id.userNameEditText);
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
+
+        Button connectButton = findViewById(R.id.connectButton);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        TextView emptyUserNameTextView = findViewById(R.id.emptyUserNameTextView);
+        TextView emptyPasswordTextView = findViewById(R.id.emptyPasswordTextView);
+        TextView incorrectTextView = findViewById(R.id.incorrectTextView);
+        TextView tryAgainTextView = findViewById(R.id.tryAgainTextView);
+
+        emptyUserNameTextView.setVisibility(View.INVISIBLE);
+        emptyPasswordTextView.setVisibility(View.INVISIBLE);
+        incorrectTextView.setVisibility(View.INVISIBLE);
+        tryAgainTextView.setVisibility(View.INVISIBLE);
+
     }
+    
 }
--- app/src/main/java/com/example/veterinaryclinic/MainActivity.java
+++ app/src/main/java/com/example/veterinaryclinic/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.veterinaryclinic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.loginButton);
+        Button signUpButton = findViewById(R.id.signUpButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignUpActivity();
+            }
+        });
+
+    }
+
+    private void openLoginActivity(){
+        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void openSignUpActivity(){
+        Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+        startActivity(intent);
     }
 }
