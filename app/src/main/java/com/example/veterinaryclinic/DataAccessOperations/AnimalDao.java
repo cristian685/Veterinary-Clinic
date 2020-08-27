5a36276f4a6a3490402fb4d9f7c2d1a7a3591a28
Cristian <cristi.popina98@gmail.com>
2020-08-27 16:16:09 +0300

    Create data access object class for Animal

---
+++ app/src/main/java/com/example/veterinaryclinic/DataAccessOperations/AnimalDao.java
@@ -0,0 +1,29 @@
+package com.example.veterinaryclinic.DataAccessOperations;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.veterinaryclinic.DatabaseEntities.Animal;
+
+import java.util.List;
+
+public interface AnimalDao {
+
+    @Insert
+    void insert(Animal animal);
+
+    @Update
+    void update(Animal animal);
+
+    @Delete
+    void delete(Animal animal);
+
+    @Query("DELETE FROM animal_table")
+    void deleteAllAnimals();
+
+    @Query("SELECT * FROM animal_table")
+    LiveData<List<Animal>> getAllAnimals();
+}
