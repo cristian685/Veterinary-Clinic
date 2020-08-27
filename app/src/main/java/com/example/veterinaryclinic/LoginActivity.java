9cdf6e6ff7524dd6874253fb309d45724b0a412e
Cristian <cristi.popina98@gmail.com>
2020-08-27 16:12:49 +0300

    Add user name and password check for user authentication

--- app/src/main/java/com/example/veterinaryclinic/LoginActivity.java
+++ app/src/main/java/com/example/veterinaryclinic/LoginActivity.java
@@ -10,26 +10,36 @@ import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String EMPTY_STRING = "";
+
+    TextView emptyUserNameTextView;
+    TextView emptyPasswordTextView;
+    TextView incorrectTextView;
+    TextView tryAgainTextView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText userNameEditText = findViewById(R.id.userNameEditText);
-        EditText passwordEditText = findViewById(R.id.passwordEditText);
+        final EditText userNameEditText = findViewById(R.id.userNameEditText);
+        final EditText passwordEditText = findViewById(R.id.passwordEditText);
 
         Button connectButton = findViewById(R.id.connectButton);
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(!checkIfAnyAccountDetailIsEmpty(userNameEditText.getText().toString(),passwordEditText.getText().toString())){
+                    
+                }
             }
         });
 
-        TextView emptyUserNameTextView = findViewById(R.id.emptyUserNameTextView);
-        TextView emptyPasswordTextView = findViewById(R.id.emptyPasswordTextView);
-        TextView incorrectTextView = findViewById(R.id.incorrectTextView);
-        TextView tryAgainTextView = findViewById(R.id.tryAgainTextView);
+        emptyUserNameTextView = findViewById(R.id.emptyUserNameTextView);
+        emptyPasswordTextView = findViewById(R.id.emptyPasswordTextView);
+        incorrectTextView = findViewById(R.id.incorrectTextView);
+        tryAgainTextView = findViewById(R.id.tryAgainTextView);
 
         emptyUserNameTextView.setVisibility(View.INVISIBLE);
         emptyPasswordTextView.setVisibility(View.INVISIBLE);
@@ -37,5 +47,37 @@ public class LoginActivity extends AppCompatActivity {
         tryAgainTextView.setVisibility(View.INVISIBLE);
 
     }
-    
+
+    private boolean checkIfAnyAccountDetailIsEmpty(String userName, String password){
+
+        boolean userNameIsEmpty = checkIfUserNameIsEmpty(userName);
+        boolean passwordIsEmpty = checkIfPasswordIsEmpty(password);
+
+        if(userNameIsEmpty || passwordIsEmpty){
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean checkIfUserNameIsEmpty(String userName){
+        if(userName.equals(EMPTY_STRING)){
+            emptyUserNameTextView.setVisibility(View.VISIBLE);
+            return true;
+        }
+
+        emptyUserNameTextView.setVisibility(View.INVISIBLE);
+        return false;
+    }
+
+    private boolean checkIfPasswordIsEmpty(String password){
+        if(password.equals(EMPTY_STRING)){
+            emptyPasswordTextView.setVisibility(View.VISIBLE);
+            return true;
+        }
+
+        emptyPasswordTextView.setVisibility(View.INVISIBLE);
+        return false;
+    }
+
 }
