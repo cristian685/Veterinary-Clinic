2375255649ef64adb41de1bddafb31fa5bdd6f43
Cristian <cristi.popina98@gmail.com>
2020-08-30 17:04:05 +0300

    a Add entities to database and change the version number

--- app/src/main/java/com/example/veterinaryclinic/Fragments/VeterinarianSignUpFragment.java
+++ app/src/main/java/com/example/veterinaryclinic/Fragments/VeterinarianSignUpFragment.java
@@ -18,6 +18,7 @@ public class VeterinarianSignUpFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_veterinarian_sign_up,container,false);
+        return view;
     }
 }
