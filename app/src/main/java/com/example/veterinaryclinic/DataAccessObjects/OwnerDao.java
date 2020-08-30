886ebfcdc16486b91241173f744d1c6ed5243f87
Cristian <cristi.popina98@gmail.com>
2020-08-30 16:48:36 +0300

    Add Owner data object access class

---
+++ app/src/main/java/com/example/veterinaryclinic/DataAccessObjects/OwnerDao.java
@@ -0,0 +1,32 @@
+package com.example.veterinaryclinic.DataAccessObjects;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.veterinaryclinic.Models.Animal;
+import com.example.veterinaryclinic.Models.Owner;
+
+import java.util.List;
+
+@Dao
+public interface OwnerDao {
+
+    @Insert
+    void insert(Owner owner);
+
+    @Update
+    void update(Owner owner);
+
+    @Delete
+    void delete(Owner owner);
+
+    @Query("DELETE FROM owner_table")
+    void deleteAllOwners();
+
+    @Query("SELECT * FROM owner_table")
+    LiveData<List<Owner>> getAllOwners();
+}
--- app/src/main/java/com/example/veterinaryclinic/Models/Owner.java
+++ app/src/main/java/com/example/veterinaryclinic/Models/Owner.java
@@ -4,13 +4,13 @@ import androidx.room.Entity;
 import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
-@Entity(foreignKeys = @ForeignKey(entity = Account.class,
-parentColumns = "idAccount",
-childColumns = "ownerId",
-onDelete = ForeignKey.CASCADE))
+@Entity(tableName = "owner_table", foreignKeys = @ForeignKey(entity = Account.class,
+        parentColumns = "idAccount",
+        childColumns = "ownerId",
+        onDelete = ForeignKey.CASCADE))
 public class Owner {
 
-    @PrimaryKey (autoGenerate = true)
+    @PrimaryKey(autoGenerate = true)
     private int ownerId;
 
     private String firstName;
@@ -23,4 +23,40 @@ public class Owner {
 
     private int idAccount;
 
+    public Owner(int ownerId, String firstName, String lastName, String emailAddress, String phoneNumber, int idAccount) {
+        this.ownerId = ownerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+        this.idAccount = idAccount;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getIdAccount() {
+        return idAccount;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
 }
